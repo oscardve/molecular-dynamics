b85e30e13ee3b83856715d20ecf693b1bfe8100a
oscardve <oscardvillarreal@gmail.com>
2016-10-12 14:13:41 -0500

    Update L-measuring-electric-fields-&-potential.tcl

--- L-measuring-electric-fields-&-potential.tcl
+++ L-measuring-electric-fields-&-potential.tcl
@@ -40,3 +40,36 @@ for {set r $r_max} {$r>0} {set r [expr $r+$dr]} { ;# for {initialization} {test}
 }
 
 close $outfile
+
+If the charge distribution is not geometrically simple, the PMEpot plugin of VMD can be used:
+
+I. Using a 150x150x500 grid (i.e. 150 points along x and y, and 500 points along z):
+package require pmepot
+set all [atomselect top "all"]
+pmepot -sel $all -frames all -xscfile box.xsc -grid {150 150 500} -dxfile eqpot.dx -ewaldfactor 0.25
+
+II. However, the output file called "eqpot.dx" is not written in a way which is plottable by programs like GNUplot. The electric potential is spread out over three columns. They may be combined into one column with the following command at the Linux terminal:
+awk '{print $1"\n"$2"\n"$3}' eqpot.dx > vv.dat #and don't forget to erase the beginning and ending
+
+III. Suppose you wish to plot the electric potential along the z axis, with x=y=0. Then, if you look at the "eqpot.dx" file, the first lines contain the location of the origin and the displacements along the x, y, z axes:
+origin -48.395 -48.395 -72.7829
+delta 0.762127 0 0
+delta 0 0.762127 0
+delta 0 0 0.762125
+
+Therefore, we need to create a column with the z values across the box:
+cstyle=\tiny]
+cz=500
+oz=-72.9533
+dz=0.762125
+fz=$(echo "$oz+$cz*$dz" | bc -l) #final value of z
+seq $oz $dz $fz > vz.dat #start, increment, end
+
+IV. Now we find the line of the "vv.dat" file containing the first value of potential along z, and extract the potential going along the z axis:
+v0=$(echo "$cz*$cy*$cx/2+$cz*$cy/2" | bc) #line containing first value
+vf=$(echo "$v0+$cz" | bc) #line containing last value
+sed -n "$v0,$vf p" vv.dat > vv2.dat #save from line $v0 to line $vf
+
+V. Finally, we merge the z values with the potential values into one file:
+paste vz.dat vv2.dat > vnah2o.dat
+
